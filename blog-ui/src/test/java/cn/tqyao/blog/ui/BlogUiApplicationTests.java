1f85727e2ea51a4c9525de741d8f5ebcf2053a14
tqyao <790461387@qq.com>
2021-03-11 16:20:26 +0800

    初始化模块blog-ui

---
+++ blog-ui/src/test/java/cn/tqyao/blog/ui/BlogUiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tqyao.blog.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
