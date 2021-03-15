be69f1913c24fe8e28e619091d9c5c6be959a715
谭钦瑶 <790461387@qq.com>
2021-03-15 22:46:13 +0800

    blog-admin初始化

--- blog-admin/src/main/java/cn/tqyao/blog/admin/BlogAdminApplication.java
+++ blog-admin/src/main/java/cn/tqyao/blog/admin/BlogAdminApplication.java
@@ -3,7 +3,7 @@ package cn.tqyao.blog.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cn.tqyao.blog")
 public class BlogAdminApplication {
 
     public static void main(String[] args) {
---
+++ blog-admin/src/main/java/cn/tqyao/blog/admin/controller/TestAdminController.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2020-2030 The author personally reserves all rights.
+ */
+package cn.tqyao.blog.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * .<br>
+ *
+ * @author tanqinyao<br>
+ * @version 1.0.0 <br>
+ * @date Create in 2021/3/15 22:40 <br>
+ */
+
+@RestController
+@RequestMapping("/admins")
+public class TestAdminController {
+
+
+}
