6bd6d3a35359f6205c3fff17b1f7d7f3b337265c
tqyao <790461387@qq.com>
2021-04-13 11:31:05 +0800

    admin 异常业务初始化

---
+++ blog-admin/src/main/java/cn/tqyao/blog/admin/mapper/LogExceptionMapper.java
@@ -0,0 +1,16 @@
+package cn.tqyao.blog.dao;
+
+import cn.tqyao.blog.entity.LogException;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 后台管理-异常日志 Mapper 接口
+ * </p>
+ *
+ * @author tqyao
+ * @since 2021-04-13
+ */
+public interface LogExceptionDao extends BaseMapper<LogException> {
+
+}
